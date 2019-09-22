0500cda946b0a3084ab7037136ab8ba03eb80fb9
siemah <m_hamiche@inptic.edu.dz>
2019-09-22 13:05:46 +0100

    passing the same data from server to client side

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import serialize from 'serialize-javascript'
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import App from '../shared/App';
@@ -15,18 +16,19 @@ app.use(express.static("public"));
 
 // handle all routes
 app.get('*', (req, res) => {
-  const _markup = renderToString(<App />);
-  res.send(getHtmlSkeleton(_markup));
+  const data='siemah';
+  const _markup = renderToString(<App data={data} />);
+  res.send(getHtmlSkeleton(_markup, data));
 });
 
-function getHtmlSkeleton(markup) {
+function getHtmlSkeleton(markup, data=null) {
   return `<!DOCTYPE html>
     <html>
       <head>
         <title>React SSR app</title>
         <script src="/bundle.js" defer></script>
+        <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
       </head>
-
       <body>
         <div id="app">${markup}</div>
       </body>
