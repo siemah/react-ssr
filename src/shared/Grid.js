2746ee745b33a13e924066007565ccb0e90d16b0
siemah <m_hamiche@inptic.edu.dz>
2019-09-22 14:43:03 +0100

    render a repos

--- src/shared/Grid.js
+++ src/shared/Grid.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
 
 export default class Grid extends Component {
+  constructor(props) {
+    super(props);
+    let repos;
+    if (__isBrowser__) {
+      repos = window.__INITIAL_DATA__;
+      delete window.__INITIAL_DATA__;
+    } else {
+      repos = props.staticContext.data;
+    }
+    this.state = {
+      repos,
+      loading: repos ? false : true,
+    }
+  }
+
+  componentDidMount() {
+
+    !this.state.repos && this.props.fetchInitialData()
+      .then(repos => this.setState({repos, loading: false,}))
+      .catch(err => this.setState({loading: false, repos: []}))
+  }
+
+
   render() {
-    const repos = this.props.data
+    const { repos, loading, } = this.state;
 
     return (
-      <ul style={{display: 'flex', flexWrap: 'wrap'}}>
-        {repos.map(({ name, owner, stargazers_count, html_url }) => (
-          <li key={name} style={{margin: 30}}>
-            <ul>
-              <li><a href={html_url}>{name}</a></li>
-              <li>@{owner.login}</li>
-              <li>{stargazers_count} stars</li>
-            </ul>
-          </li>
-        ))}
+      <ul style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {
+          loading
+            ? <h4>Loading ..</h4>
+            : (
+              repos.map(({ name, owner, stargazers_count, html_url }) => (
+                <li key={name} style={{ margin: 30 }}>
+                  <ul>
+                    <li><a href={html_url}>{name}</a></li>
+                    <li>@{owner.login}</li>
+                    <li>{stargazers_count} stars</li>
+                  </ul>
+                </li>
+              ))
+            )
+        }
       </ul>
     )
   }
