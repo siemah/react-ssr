39474592d3ca6fbba5b8a6c3c09c812a106a3da5
siemah <m_hamiche@inptic.edu.dz>
2019-09-23 17:14:46 +0100

    Book 2 render q curren book details

--- src/shared/Book.js
+++ src/shared/Book.js
@@ -1,68 +1,54 @@
 import React, { useState, useEffect, } from 'react'
 import Helmet from "react-helmet"
 
-export default class  Book extends React.Component {
+const Book = (props) => {
+  let _isMount = true;
+  let book;
 
-  constructor(props) {
-    super(props);
-    let books;
-    if (__isBrowser__) {
-      books = window.__INITIAL_DATA__;
-      delete window.__INITIAL_DATA__;
-    } else {
-      books = props.staticContext.data;
-    }
-    this.state = {
-      books: books,
-      loading: books ? false : true,
-    }
+  if (__isBrowser__) {
+    book = window.__INITIAL_DATA__;
+    delete window.__INITIAL_DATA__;
+  } else {
+    book = props.staticContext.data;
+  }
+  console.log(book)
+  const [bookstate, setBookstate] = useState({
+    loading: book ? false : true,
+    book,
+  });
+
+  const fetchBooks = function () {
+    setBookstate({ loading: true })
+    props.fetchInitialData(props.match.params.id)
+      .then(book => _isMount && setBookstate({ loading: false, book }))
+      .catch(err => _isMount && setBookstate({ loading: false, books: [], err: err.message }))
   }
 
-  componentDidMount() {
-    // to avoid any state change when component unmounted
-    this._isMount = true;
-    // fethc books if the data not exist
-    // that mean this instruction when 
-    // react take over rendering from server
-    !this.state.books && this.fetchBooks();
-  }
+  useEffect(() => {
+    !bookstate.book && fetchBooks();
+    return () => {
+      _isMount = false;
+    };
+  }, []);
 
-  componentWillUnmount() {
-    this._isMount = false;
-  }
-  
+  if (bookstate.loading) return <h2>Loading ..</h2>
 
-  fetchBooks() {
-    this.setState({loading: true})
-    this.props.fetchInitialData()
-      .then(books => this._isMount && this.setState({loading: false, books}))
-      .catch(err => this._isMount && this.setState({loading: false, books: [], err: err.message}))
-  }
-  
-
-  render() {
-    let { loading, books, } = this.state;
-
-    return (
-      <div style={{margin: '5%'}}>
-        <Helmet>
-          <title>Find Books you search for it</title>
-          <meta name='description' content='find any kind of book no matter who the author' />
-          <meta property='og:title' content='find any kind of book no matter who the author' />
-        </Helmet>
-        <h1>Book list</h1>
-        {
-          loading && <h4>Loading ..</h4>
-        }
-        {
-          books && books.map(({_id, description, title}) => (
-            <div key={_id}>
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
-          ))
-        }
+  return (
+    <div>
+      <Helmet>
+        <title>{bookstate.book.title}</title>
+        <meta name='description' content={bookstate.book.description} />
+        <meta property='og:title' content={bookstate.book.description} />
+      </Helmet>
+      <div id={bookstate.book._id}>
+        <h2>{bookstate.book.title}</h2>
+        <p>{bookstate.book.description}</p>
+        <blockquote>
+          ISBN: {bookstate.book.isbn}
+        </blockquote>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default Book
