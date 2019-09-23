efb0b5b581e0a1b62fedbf0cfc0e53b8eea25e2d
siemah <m_hamiche@inptic.edu.dz>
2019-09-23 17:15:05 +0100

    add Book 2 routes central

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,9 +1,10 @@
 // shared/routes.js
 import Home from './Home'
 import Grid from './Grid'
-import Book from './Book'
+import BooksList from './BooksList'
 import { fetchPopularRepos } from './api'
 import { fetchBook } from './api'
+import Book from './Book'
 
 const routes =  [
   {
@@ -19,9 +20,23 @@ const routes =  [
     ),
   },
   {
+    path: '/books/:id',
+    component: BooksList,
+    fetchInitialData: (book=null) => fetchBook(
+      book.split('/').pop()
+    ),
+  },
+  {
     path: '/books',
+    component: BooksList,
+    fetchInitialData: () => fetchBook(),
+  },
+  {
+    path: '/book/:id',
     component: Book,
-    fetchInitialData: (book = null) => fetchBook(),
+    fetchInitialData: (book = null) => fetchBook(
+      book.split('/').pop()
+    ),
   }
 ]
 
