c1e4e59636d2cabedc359b5b3e23596826efff52
siemah <m_hamiche@inptic.edu.dz>
2019-09-24 15:05:12 +0100

    handle login process by submiting auth form

--- src/shared/Login.js
+++ src/shared/Login.js
@@ -1,32 +1,53 @@
 import React, { useState, } from 'react'
 import Helmet from 'react-helmet'
+import AuthContext from './context/Auth';
 
-export default function Login() {
+export default function Login({ fetchCredentials, }) {
   const [credentials, setCredentials] = useState({
     email: null,
     password: null,
   });
+  const [state, setState] = useState({
+    loading: false,
+    message: null,
+  })
 
   const _onChange = ({ target }) => setCredentials({
     ...credentials, 
     [target.name]: target.value
   });
 
-  const _onSubmit =  ({ preventDefault, }) => {
-    preventDefault();
-    
+  const _onSubmit =  (evn) => {
+    evn.preventDefault();
+    setState({ loading: true, message: null})
+    fetchCredentials(evn.target.action, credentials)
+      .then(_res => {
+        setState({ loading: false, message: null})
+      })
+      .catch(({ message }) => {
+        setState({ loading: false, message})
+      })
   }
 
   return (
-    <form action='#' onSubmit={_onSubmit}>
+    <form action='/auth/login' onSubmit={_onSubmit}>
       <Helmet>
         <title>Login</title>
         <meta name='description' content='Login to SSR account' />
       </Helmet>
       <h1>Login</h1>
-      <input name='email' placeholder='Email address' /><br />
-      <input name='password' type='password' placeholder='Your password' /><br />
-      <button type='submit'>Let me in</button>  
+      {
+        state.message && <mark>{state.message}</mark>
+      }
+      <input onChange={_onChange} name='email' placeholder='Email address' /><br />
+      <input onChange={_onChange} name='password' type='password' placeholder='Your password' /><br />
+      <button disabled={!!state.loading} type='submit'>
+        {
+          !!state.loading
+            ? 'Loading ..'
+            : 'Let me in'
+        }
+      </button>  
     </form>
   )
 }
